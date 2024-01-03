d491c251f4399ab49822d243a43bb460c2d35149
Ekeocha-ibe victor chinemerem <victorekeocha016@gmail.com>
2024-01-03 20:44:10 +0100

    fixed section7

--- app/components/homepage_components/Section7.jsx
+++ app/components/homepage_components/Section7.jsx
@@ -7,9 +7,9 @@ const playFair = Playfair_Display({ weight: "400", subsets: ["latin"] });
 const Section7 = () => {
   return (
     <div className="w-full h-[70vh] bg-slate-500 bg-[url('/images/event3.jpg')] bg-cover mt-10">
-        <div className="w-full h-full bg-[#000000a8] flex flex-col justify-center items-center text-white gap-y-10">
+        <div className="w-full h-full bg-[#000000a8] flex flex-col justify-center items-center text-white gap-y-10 px-6">
           <p className={`lg:text-6xl text-4xl ${playFair.className} text-center`}>Unlock Connection & Excitement</p>
-          <p className={`text-2xl ${playFair.className} text-center lg:px-60`}>Join Now to Experience Exclusive Events, Thriving Friendships, and Unforgettable</p>
+          <p className={`lg:text-2xl text-xl ${playFair.className} text-center lg:px-60`}>Join Now to Experience Exclusive Events, Thriving Friendships, and Unforgettable</p>
 
           <div className="w-[18rem] h-[3.3rem] border-2 bg-white  rounded-xl flex justify-center items-center gap-x-2 text-black">
             <p>Join Now</p>
