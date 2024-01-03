dddb2d3260c6d21e3337d92c571a969f1818a0b2
Ekeocha-ibe victor chinemerem <victorekeocha016@gmail.com>
2024-01-03 20:59:07 +0100

    fixed section6

--- app/components/homepage_components/Section6.jsx
+++ app/components/homepage_components/Section6.jsx
@@ -49,12 +49,12 @@ const Section6 = () => {
       name: "Maryjane",
       role: "Student",
       comment:
-        "I recently attended an event at FCSC and was incredibly satisfied with the experience. The event was perfectly organized and ran smoothly. I would highly recommend FCSC for any event!",
+        "I recently attended an event at FCSC and was incredibly satisfied with the experience.",
     },
   ];
 
   return (
-    <div className="w-full lg:h-screen h-[125vh] bg-orange mt-10">
+    <div className="w-full lg:h-screen h-[90vh] bg-orange my-16">
       <div className="w-full lg:h-[40%] h-[30%] flex flex-col justify-center items-center lg:px-64 gap-y-5 bg-green">
         <p className="text-4xl font-bold text-center">Hear From Our Members</p>
         <p className="lg:text-xl text-lg text-center">
@@ -62,10 +62,10 @@ const Section6 = () => {
         </p>
       </div>
 
-      <div className="w-full lg:h-[60%] h-[70%] flex justify-center items-center ">
-        <div className="lg:w-[80%] w-[90%] lg:h-[90%] h-full bg-[#FFDAB9] flex lg:flex-row flex-col gap-x-6 rounded-md overflow-hidden lg:p-0 p-2">
+      <div className="w-full lg:h-[60%] h-[70%] flex justify-center items-end bg-yellow">
+        <div className="lg:w-[80%] w-[90%] lg:h-[90%] h-[90%] bg-[#FFDAB9] flex justify-center items-center lg:flex-row flex-col gap-x-6 rounded-md overflow-hidden lg:p-0 p-2">
           <div
-            className="lg:w-[30%] w-full lg:h-full h-1/2 bg-slate-300 lg:rounded-r-[50%] lg:rounded-none rounded-full overflow-hidden"
+            className="lg:w-[30%] w-[10rem] lg:h-full h-[10rem] bg-slate-300 lg:rounded-r-[50%] lg:rounded-none rounded-full overflow-hidden"
             style={{
               backgroundImage: `url(${testimonials[index].image})`,
               backgroundSize: "cover",
@@ -108,13 +108,13 @@ const Section6 = () => {
               </p>
             </div>
             <div
-              className={`w-full h-[20%] bg-pink ${playFair.className} flex px-3`}
+              className={`w-full lg:h-[30%] bg-pink ${playFair.className} flex justify-center items-center px-3`}
             >
               <div className="w-1/2 h-full  bg-blue">
                 <p className="text-[1.6rem]">{testimonials[index].name}</p>
                 <p>{testimonials[index].role}</p>
               </div>
-              <div className="w-1/2 h-full bg-yellow flex justify-end items-center gap-x-5">
+              <div className="w-1/2 h-full bg-yellow flex justify-end items-center gap-x-5 ">
                 <div
                   className="w-10 h-10 rounded-full bg-zinc-600 text-3xl font-bold flex justify-center items-center text-white cursor-pointer"
                   onClick={decrementIndex}
